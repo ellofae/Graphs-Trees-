7a6cc8b764f766d1a4c5c52bdbf6f088ded14389
ellofae <94999672+ellofae@users.noreply.github.com>
2022-12-24 23:08:24 +0300

    Create Globals.cs

---
+++ N-ary Tree (another one)/Globals.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2_BinaryTree
+{
+    public static class Globals
+    {
+        public static int sonsAmount;
+        public static List<Node> nodes = new List<Node>();
+    }
+}
